7c6f83e6b74a1a0f3a199af07c0c50e47e5fdcc6
Beef <ethandavidbarrow@outlook.com>
2020-11-15 22:20:08 +0000

    start on rewrite, use WinCMDold for now!

--- WinCMD.bat
+++ WinCMD.bat
@@ -1,97 +1,49 @@
-rem ===Default Values and Initial Load===
-@echo off 
+rem Initial Load
+
+@echo off
+setlocal enabledelayedexpansion
 cls 
-Set name=WinCMD
-Set version=1.0
-Set author=Beef
+Set "name=WinCMD"
+Set "version=1.0"
+Set "author=Beef"
 
-title %name%, an open-source command line
-echo Welcome to %name%, an open-source command line
-echo. 
+title "%name%", an open-source command line
+echo Welcome to "%name%", an open-source command line. Everything you see here is a beta product and is subject to change.
 
-rem ===Declares what a command is===
-:command 
+rem Command Handler
 
-title %name%, an open-source command line
-echo. 
+:command
+
+title "%name%", an open source command line
+echo.
 color a
-Set /p input=%USERNAME%@%name%:~%CD%$ 
+set /p "input=%USERNAME%@%name%:~%CD%$ "
 
-title %name% - %input%
-rem ===Loading Commands===
-IF %input%==help (
-goto help
-)
-IF %input%==doscmd (
-goto doscmd
-)
-IF %input%==version (
-goto version
-)
-IF %input%==new (  
-goto new
-)
-IF %input%==credits (
-goto credits
-)
-IF %input%==defaultbrowser (
-goto defaultbrowser
-)
-) ELSE (
-set error=cmdinvalid
-goto ERROR
-)
-title %name%, an open-source command line
-goto command
+title "%name% - %input%"
 
-rem ===Error Handler===
-:ERROR
+rem Command Declarations
 
-echo ===%name% Error Handler===
-IF %error%==cmdinvalid (
-echo That was not a command. If you're looking to run DOS commands, type doscmd.
-goto command
-) ELSE (
-echo invalid error code, if you see this message, contact a dev!
-goto command
+if %input%=="help" (
+    goto help
+)
+) else (
+    set "error=invalidcommand"
+    goto errorhandler
 )
 
+rem Error Handler
 
-rem ===Declaring Commands===
+:errorhandler
+echo ==="%name%" Error Handler===
+if %error%=="invalidcommand" (
+    echo That was not a valid command.
+    goto command
+)
+
+rem Command Functions
 
 :help
-echo ===%name% Commands===
-echo help - Displays this command list along with the default DOS/CMD commands.
-echo version - Displays the current %name% version.
-echo new - Makes a new window of %name%.
-echo doscmd - Lets you run MS-DOS/Command Prompt commands. This can be used to break WinCMD.
-echo defaultbrowser - Starts your default browser. (Definitely not a shameless plug, nope...)
-echo credits - Displays the credits of this program.
+echo ==="%name%" Commands===
+echo help - Displays this command list.
 echo.
-goto command
-
-:doscmd
-Set /p doscmd="Input DOS Command - <%CD%>"
-%doscmd%
-goto command
-
-:version
-echo %name% %version%
-goto command
-
-:new
-start WinCMD.bat
-goto command
-
-:credits
-echo ===%name% Credits===
-echo %name% %version%
-echo Created by %author%
-IF not '%name%'=='WinCMD' (
-echo Built upon the WinCMD core by Beef
-)
-goto command
-
-:defaultbrowser
-start https://github.com/Beefers/WinCMD
 goto command
