1e8274bb9722fdec67486489c09bcb9ed08fe53c
Beef <ethandavidbarrow@outlook.com>
2020-11-20 11:33:07 +0000

    [rewrite] plenty of changes, safe to use

--- WinCMD.bat
+++ WinCMD.bat
@@ -5,15 +5,16 @@ rem Variables
 rem Verbose Mode - Displays debug messages. Default value is "false" and options are: "false, true"
 Set "verbosemode=false"
 
+rem Experiments - Enables experimental functionality e.g. commands. Default value is "false" and options are: "false, true"
+Set "enableexperiments=false"
+
 @echo off
-setlocal enabledelayedexpansion
 cls 
 Set "name=WinCMD"
 Set "version=0.1 rewrite"
 Set "author=Beef"
 Set "slogan=an open-source command line"
 
-title %name%, %slogan%
 echo Welcome to %name%, %slogan%.
 echo This is a beta product.
 echo Everything you see here is subject to change at any time, without warning.
@@ -29,7 +30,7 @@ rem Command Handler
 if %verbosemode%==true (
     echo DEBUG: Command handler loaded successfully
 )
-title %name%, an open source command line
+title %name%, %slogan%
 echo.
 color a
 set /p "input=%USERNAME%@%name%:~%CD%$ "
@@ -45,16 +46,22 @@ if %input%==test (
     goto test
 )
 if %input%==credits (
-    goto test
+    goto credits
 )
 if %input%==reload (
-    WinCMD.bat
+    cmd.exe /C %~0 parameters
 )
 if %input%==defaultbrowser (
     goto defaultbrowser
 )
-if %input%==doscmd (
-    goto doscmd
+if %input%==new (
+    goto new
+)
+if %input%==gittest (
+    goto gittest
+)
+if %input%==cmd (
+    goto cmd
 )
 ) else (
     set "error=invalidcommand"
@@ -80,12 +87,16 @@ echo ===%name% Commands===
 echo help - Displays this command list.
 echo credits - Displays program credits.
 echo reload - Reloads WinCMD. Useful for debugging.
+echo defaultbrowser - Starts your default browser. (Definitely not a shameless plug, nope...)
+echo doscmd - Allows you to run MS-DOS/Command Prompt commands. Has a new look, courtesy of the rewrite.
+echo new - Launches a new window of WinCMD. Now using an improved method!
 echo.
 goto command
 
 :credits
 echo === %name%, %slogan%. ===
 echo Created by %author%
+echo Version %version%
 IF not '%name%'=='WinCMD' (
 echo Built upon the WinCMD core by Beef
 )
@@ -100,14 +111,40 @@ echo This is a test.
 echo.
 goto command
 
-:doscmd
-cls
-color f
+:new
+start cmd.exe /C %~0 parameters
+goto command
+
+:gittest
+git --version
+if errorlevel 1 (
+    echo.
+    echo git is not installed.
+)
+if not errorlevel 1 (
+    echo git is installed.
+)
+) else (
+    echo unable to determine whether git is installed.
+)
+goto command
+
+:update
+echo this is not complete yet.
+
+:cmd
+title [WinCMD] Command Prompt
+color 7
 for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
+echo.
 echo Microsoft Windows [Version %VERSION%]
 echo (c) %author% Corporation. No rights reserved.
 echo This Command Prompt is a visual recreation of Windows Command Prompt, running under WinCMD.
+if %verbosemode%==true (
+    echo DEBUG: CMD recreation loaded successfully
+)
 echo.
-Set /p "doscmd=%CD%>"
-%doscmd%
+Set /p "cmd=%CD%>"
+title [%name%] Command Prompt - %cmd%
+%cmd%
 goto command
